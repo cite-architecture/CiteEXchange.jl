a962ad453aac0f272fd8a3a112d31ad984a4b4ef
Neel Smith <dnsmith.neel@gmail.com>
2021-08-01 19:06:40 -0400

    Add constructor from URL

--- src/file.jl
+++ src/file.jl
@@ -7,3 +7,14 @@ function fromfile(fname)
     cex =  open(f->read(f, String), fname)
     blocks(cex)
 end
+
+
+"""Parse CEX data from a URL into a group of `Block`s.
+
+$(SIGNATURES)
+"""
+function fromurl(url)
+    data = HTTP.get(url).body |> String
+    #cex =  open(f->read(f, String), fname)
+    blocks(data)
+end
