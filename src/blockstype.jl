1b03c66fb29785d164ca02a8e6918d4854390205
Neel Smith <dnsmith.neel@gmail.com>
2021-12-30 11:25:04 -0500

    Define `Block` type

---
+++ src/blockstype.jl
@@ -0,0 +1,85 @@
+"""A single block of CEX data."""
+struct Block
+    label
+    lines
+end
+
+"""Override `==` for `Block`.
+
+$(SIGNATURES)
+"""
+function ==(b1::Block, b2::Block)
+    b1.label == b2.label && b1.lines == b2.lines
+end
+
+"""Serialize `b` to CEX format.
+$(SIGNATURES)
+"""
+function blocktocex(b::Block)
+    join(["!#", b.label, "\n"]) * join(b.lines, "\n") * "\n"
+end
+
+"""Find list of unique block types in a group.
+
+$(SIGNATURES)
+"""
+function blocktypes(blockgroup::Vector{Block})::Vector{AbstractString}
+    map(blk -> blk.label, blockgroup) |> unique
+end
+
+
+"""Find list of unique block types in a group.
+
+$(SIGNATURES)
+"""
+function blocktypes(cexsrc::AbstractString)::Vector{AbstractString}
+    blockgroup = blocks(cexsrc)
+    map(blk -> blk.label, blockgroup) |> unique
+end
+
+
+
+"""Determine block type from first line of a CEX block.
+
+$(SIGNATURES)
+"""
+function blocktype(s::AbstractString)::Union{AbstractString, Nothing}
+    validtypes = [
+        "cexversion",
+        "citelibrary",
+        "ctsdata",
+        "ctscatalog",
+        "citecollections",
+        "citeproperties",
+        "citedata",
+        "imagedata",
+        "relations", # deprecated
+        "datamodels",
+        "citerelationset",
+        "relationsetcatalog"
+    ]
+    if s in validtypes
+        s
+    else 
+        @warn "Unrecognized block type: $s. Omitting."
+        nothing
+    end
+end
+
+
+"""Find CEX version for a block group.
+
+$(SIGNATURES)
+
+Return nothing if no version specified.
+"""
+function cexversion(group)::Union{VersionNumber, Nothing}
+    versiondata = datafortype("cexversion", group)
+    if length(versiondata) != 1
+        @warn "Error: $(length(versiondata)) lines of version data found."
+        nothing
+    else
+        versiondata[1] |> VersionNumber
+    end
+end
+
--- test/test_blocks.jl
+++ test/test_blocks.jl
@@ -1,101 +1,11 @@
+@testset "Test `Block` type equality, identification of blocktypes and cex conversion" begin
+    blockgroup = blocks(joinpath("assets", "burneyex.cex"), CiteEXchange.FileReader)
+    @test blocktypes(blockgroup) == ["ctscatalog", "ctsdata"]
+    @test blockgroup[1] != blockgroup[2] 
+    dupecatalog = blockgroup[1]
+    @test blockgroup[1] == dupecatalog
 
-@testset "Test CEX parsing" begin
-    cex = """
-    
-#!citecollections
-URN#Description#Labelling property#Ordering property#License
-urn:cite2:hmt:va_signs.v1:#Occurrences of Aristarchan critical signs in the Veentus A manuscript#urn:cite2:hmt:va_signs.v1.label:#urn:cite2:hmt:va_signs.v1.sequence:#CC-attribution-share-alike
-
-#!citeproperties
-Property#Label#Type#Authority list
-urn:cite2:hmt:va_signs.v1.urn:#URN#Cite2Urn#
-urn:cite2:hmt:va_signs.v1.label:#Name#String#
-urn:cite2:hmt:va_signs.v1.passage:#Iliad line#CtsUrn#
-urn:cite2:hmt:va_signs.v1.critsign:#Critical sign#Cite2Urn#
-urn:cite2:hmt:va_signs.v1.sequence:#Sequence#Number#
-
-#!citedata
-urn#label#passage#critsign#sequence
-urn:cite2:hmt:va_signs.v1:cs0#diple on Iliad 1.2#urn:cts:greekLit:tlg0012.tlg001.msA:1.2#urn:cite2:hmt:critsigns.v1:diple#0
-"""    
-    blks = blocks(cex)
-    @test length(blks) == 3
-    @test blocktypes(blks) == ["citecollections", "citeproperties", "citedata"]
+    expectedcex = "!#ctscatalog\nurn|citationScheme|groupName|workTitle|versionLabel|exemplarLabel|online|language\nurn:cts:greekLit:tlg5026.burney86.hmt:|book, scholion|Scholia to the Iliad|Main scholia to the Iliad of British Library, Burney 86|British Library, Burney 86||true|grc\nurn:cts:greekLit:tlg5026.burney86int.hmt:|book,scholion,section|Scholia to the Iliad|Interior scholia of British Library, Burney 86|British Library, Burney 86||true|grc\n"
+    @test CiteEXchange.blocktocex(dupecatalog)== expectedcex
 end
 
-
-@testset "Test bad input" begin
-    cex = "#!nontype\nCEX data\n"
-    blks = blocks(cex)
-    @test isempty(blks)
-end
-
-@testset "Test supporting comments" begin
-    cex = """#!ctscatalog
-
-    // Complete catalog for a single citable text: an edition
-    // of scholia in a manuscript of the *Iliad*.  There is no
-    // specific exemplar of this edition.
-    
-    urn#citationScheme#groupName#workTitle#versionLabel#exemplarLabel#online#lang
-    urn:cts:greekLit:tlg5026.msA.hmt:#book,comment,section#Scholia Vetera in Iliadem#Main scholia to Venetus A#Homer Multitext##true#grc
-    """
-    blockgroup = blocks(cex)
-    expected = [
-        "urn#citationScheme#groupName#workTitle#versionLabel#exemplarLabel#online#lang",
-        "urn:cts:greekLit:tlg5026.msA.hmt:#book,comment,section#Scholia Vetera in Iliadem#Main scholia to Venetus A#Homer Multitext##true#grc"
-    ]
-    blockgroup[1].lines == expected
-end
-
-@testset "Test collecting data from group" begin
-    cex = """
-    
-#!citecollections
-URN#Description#Labelling property#Ordering property#License
-urn:cite2:hmt:va_signs.v1:#Occurrences of Aristarchan critical signs in the Veentus A manuscript#urn:cite2:hmt:va_signs.v1.label:#urn:cite2:hmt:va_signs.v1.sequence:#CC-attribution-share-alike
-
-#!citeproperties
-Property#Label#Type#Authority list
-urn:cite2:hmt:va_signs.v1.urn:#URN#Cite2Urn#
-urn:cite2:hmt:va_signs.v1.label:#Name#String#
-urn:cite2:hmt:va_signs.v1.passage:#Iliad line#CtsUrn#
-urn:cite2:hmt:va_signs.v1.critsign:#Critical sign#Cite2Urn#
-urn:cite2:hmt:va_signs.v1.sequence:#Sequence#Number#
-
-#!citedata
-urn#label#passage#critsign#sequence
-urn:cite2:hmt:va_signs.v1:cs0#diple on Iliad 1.2#urn:cts:greekLit:tlg0012.tlg001.msA:1.2#urn:cite2:hmt:critsigns.v1:diple#0
-""" 
-    blockgroup = blocks(cex)
-    data = datafortype("citedata", blockgroup)
-    expected = [
-        "urn#label#passage#critsign#sequence",
-        "urn:cite2:hmt:va_signs.v1:cs0#diple on Iliad 1.2#urn:cts:greekLit:tlg0012.tlg001.msA:1.2#urn:cite2:hmt:critsigns.v1:diple#0"
-        ]
-    @test data == expected
-end
-
-
-@testset "Test versioning" begin
-    cex = """
-#!cexversion
-
-// note: currently using version 3.1
-3.1
-
-#!citedata
-urn#label#passage#critsign#sequence
-urn:cite2:hmt:va_signs.v1:cs0#diple on Iliad 1.2#urn:cts:greekLit:tlg0012.tlg001.msA:1.2#urn:cite2:hmt:critsigns.v1:diple#0
-"""
-    blkgroup = blocks(cex)
-    @test cexversion(blkgroup) == v"3.1"
-end
-
-
-@testset "Test CTS data block without header" begin
-    f = "assets/textblocks.cex"
-    blks = CiteEXchange.blocks(f, FileReader)
-    datalines = datafortype("ctsdata", blks)
-    @test length(datalines) == 4
-end
